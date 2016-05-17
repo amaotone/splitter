b8c971b88448d3013075ec956c86f8e6d91cccc6
Amane <amane.suzu@gmail.com>
2016-05-17 23:48:11 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# timer
+
+speedrun timer
---
+++ scripts/app.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+    var timer = new Vue({
+        el: '#timer',
+        data: {
+            segments: [
+                {name: "Asylum Demon", time: 0},
+                {name: "Get Weapon", time: 0},
+                {name: "Gargoyles", time: 0},
+                {name: "Quelaag", time: 0},
+                {name: "Sif", time: 0},
+                {name: "Iron Golem", time: 0},
+                {name: "O&S", time: 0},
+                {name: "Four Kings", time: 0},
+                {name: "Seath", time: 0},
+                {name: "Pinwheel", time: 0},
+                {name: "Nito", time: 0},
+                {name: "Firesage", time: 0},
+                {name: "Centipede", time: 0},
+                {name: "BoC", time: 0},
+                {name: "Gwyn", time: 0}
+            ]
+        },
+        methods: {
+            split: function() {
+
+            },
+            pause: function() {
+
+            },
+            reset: function() {
+
+            },
+            addSegment: function() {
+            },
+            removeSegment: function() {
+
+            }
+        }
+    });
+};
