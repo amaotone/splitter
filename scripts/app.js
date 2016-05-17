854988cf703bfd3b4d293ed29abbf1155537fb7a
Amane <amane.suzu@gmail.com>
2016-05-18 01:18:10 +0900

    Enable split/unsplit/pause/reset

--- scripts/app.js
+++ scripts/app.js
@@ -2,39 +2,73 @@ window.onload = function () {
     var timer = new Vue({
         el: '#timer',
         data: {
+            run: false,
+            pointer: 0,
             segments: [
-                {name: "Asylum Demon", time: 0},
-                {name: "Get Weapon", time: 0},
-                {name: "Gargoyles", time: 0},
-                {name: "Quelaag", time: 0},
-                {name: "Sif", time: 0},
-                {name: "Iron Golem", time: 0},
-                {name: "O&S", time: 0},
-                {name: "Four Kings", time: 0},
-                {name: "Seath", time: 0},
-                {name: "Pinwheel", time: 0},
-                {name: "Nito", time: 0},
-                {name: "Firesage", time: 0},
-                {name: "Centipede", time: 0},
-                {name: "BoC", time: 0},
-                {name: "Gwyn", time: 0}
+                {name: "Asylum Demon", time: 0, lap: 0},
+                {name: "Get Weapon", time: 0, lap: 0},
+                {name: "Gargoyles", time: 0, lap: 0},
+                {name: "Quelaag", time: 0, lap: 0},
+                {name: "Sif", time: 0, lap: 0},
+                {name: "Iron Golem", time: 0, lap: 0},
+                {name: "O&S", time: 0, lap: 0},
+                {name: "Four Kings", time: 0, lap: 0},
+                {name: "Seath", time: 0, lap: 0},
+                {name: "Pinwheel", time: 0, lap: 0},
+                {name: "Nito", time: 0, lap: 0},
+                {name: "Firesage", time: 0, lap: 0},
+                {name: "Centipede", time: 0, lap: 0},
+                {name: "BoC", time: 0, lap: 0},
+                {name: "Gwyn", time: 0, lap: 0}
             ]
         },
         methods: {
-            split: function() {
-
+            update: function () {
+                if (this.run) {
+                    current = this.segments[this.pointer];
+                    current.time += 1;
+                    current.lap += 1;
+                }
             },
-            pause: function() {
+            split: function () {
+                first = this.segments[0];
+                if (!this.run && first.time == 0) {
+                    this.run = true;
+                }
+                else if (this.run) {
+                    current = this.segments[this.pointer];
+                    next = this.segments[this.pointer + 1];
 
+                    next.time += current.time;
+                    this.pointer += 1;
+                }
             },
-            reset: function() {
+            unsplit: function () {
+                if (this.run && this.pointer > 0) {
+                    current = this.segments[this.pointer];
+                    previous = this.segments[this.pointer - 1];
 
+                    previous.time = current.time;
+                    previous.lap += current.lap;
+                    current.time = 0;
+                    current.lap = 0;
+                    this.pointer -= 1;
+                }
             },
-            addSegment: function() {
+            pause: function () {
+                this.run = !this.run;
             },
-            removeSegment: function() {
-
+            reset: function () {
+                this.segments.forEach(function (segment) {
+                    segment.time = 0;
+                    segment.lap = 0;
+                });
+                this.pointer = 0;
+                this.run = false;
             }
         }
     });
+    setInterval(function () {
+        timer.update();
+    }, 100);
 };
